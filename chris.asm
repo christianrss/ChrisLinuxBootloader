a06e7b61827baef5e9146fda1706b1e554cc0e08
Christian Rafael <christianparadix@gmail.com>
2025-10-29 22:01:00 -0300

    Writing bootloader

--- chris.asm
+++ chris.asm
@@ -6,7 +6,53 @@ global _start
 
 section .bootloader
     _start:
+    stack:
+        jmp main
+
+    str:
+        db "Hello world",0x0a,0x0d,0x00
+
+    print:
+        push bp
+        mov bp,sp
+        mov si,[bp+4]
+        mov ax,0x0e00
+        xor bx,bx
+
+    .loop:
+        lodsb
+        or al, al
+        jz .end
+        int 0x10
+        jmp .loop
+    .end:
+        mov sp,bp
+        pop bp
+        sub sp,0x02
+        ret
+
+    main:
+        cli
+        mov ax,cs
+        mov ss,ax
+        mov ds,ax
+        mov es,ax
+        mov ax,stack
+        sub ax,0x02
+        mov sp,ax
+        sti
+
+        mov ax,str
+        push ax
+        call print
+        jmp halt
         nop
+
+    halt:
+        hlt
+    .loop
+        nop
+        jmp .loop
 
 section .ptable
     first:
