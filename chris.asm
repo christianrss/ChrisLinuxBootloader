8be8dcc45ae72c60c3c18a1b5bec378d616c2539
Christian Rafael <christianparadix@gmail.com>
2025-10-29 21:43:42 -0300

    Define partitions

--- chris.asm
+++ chris.asm
@@ -1,4 +1,5 @@
 ; chris bootloader
+%include "chris.h"
 bits 16
 
 global _start
@@ -8,7 +9,22 @@ section .bootloader
         nop
 
 section .ptable
-    tmp     times 64 db 0x00
+    first:
+        istruc partition
+            at partition,   times 16 db 0x01
+        iend
+    second:
+        istruc partition
+            at partition,   times 16 db 0x02
+        iend
+    third:
+        istruc partition
+            at partition,   times 16 db 0x03
+        iend
+    fourth:
+        istruc partition
+            at partition,   times 16 db 0x04
+        iend
 
 section .sign
     ; Valid bootsector signature bytes
